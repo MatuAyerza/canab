89d386dc6e716ac5b2a9f123451052ced7ac6d0e
MatuAyerza <matu.ayerza@gmail.com>
2021-09-07 22:22:16 -0300

    first version

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Canab' });
+  res.render('index', { title: 'CANAB' });
 });
 
 module.exports = router;
